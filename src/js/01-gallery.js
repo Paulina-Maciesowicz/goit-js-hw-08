80744773fc4673389670908aa4e458722e997867
Paulina-Maciesowicz <paulinamaa@gmail.com>
2023-02-25 19:53:42 +0100

    Update

---
+++ src/js/01-gallery.js
@@ -0,0 +1,24 @@
+import { galleryItems } from './gallery-items.js';
+
+// Change code below this line
+
+console.log(galleryItems);
+
+const markup = galleryItems
+  .map(
+    image =>
+      `<a class="gallery__item" href=${image.original}>
+        <img
+          class="gallery__image"
+          src=${image.preview}
+          alt="${image.description}"
+        />
+      </a>`
+  )
+
+  .join('');
+document.querySelector('ul.gallery').insertAdjacentHTML('afterbegin', markup);
+
+(function () {
+  new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData: 'alt' });
+})();
